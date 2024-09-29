8bf7b7f507f18200381d80829a14f5c1d1eafd68
BijanProgrammer <bijaneisapour@gmail.com>
2024-09-29 13:56:53 +0330

    [ADD] dom manipulation

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello, friend!');
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', () => {
+	h1.innerHTML = 'Hello to you as well!';
+});
